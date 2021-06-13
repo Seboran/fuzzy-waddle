7134703bcec7ceb5c589aaabcf784f4e4e8b283a
Nirina Rabeson <nirinarabeson@gmail.com>
2021-06-13 10:39:43 +0200

    added ping command

--- src/app.js
+++ src/app.js
@@ -1,22 +1,52 @@
-import "../init.js";
-import { Client } from "discord.js";
+import '../init.js'
+import {
+  Client,
+  MessageActionRow,
+  MessageButton,
+  Permissions,
+} from 'discord.js'
 
-const client = new Client();
+const client = new Client({ intents: ['GUILDS', 'GUILD_MESSAGES'] })
 
-client.on('ready', () => {
-	console.log(`Logged in as ${client.user.tag}!`);
-});
+client.on('ready', async () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+  const data = [
+    {
+      name: 'ping',
+      description: 'Replies with Pong!',
+    },
+  ]
 
-client.on('interaction', async interaction => {
-	if (!interaction.isCommand()) return;
-	if (interaction.commandName === 'ping') {
-		await interaction.reply('Pong!');
-	}
-});
+  // Add command to DMs
+  await client.application?.commands.set([])
 
-const TOKEN = process.env.DISCORD_TOKEN;
+  // Add command to each guild
+  await Promise.all(
+    client.guilds.cache.map(async (guild) => {
+      await guild.commands
+        .set(data)
+        .then(() => console.log(`Set commands in ${guild.name}`))
+        .catch((e) => {
+          console.error(`Could not add command to ${guild.name}`)
+          console.error(e)
+        })
+    }),
+  )
+
+  console.log('Set all commands')
+})
+
+client.on('interaction', async (interaction) => {
+  if (!interaction.isCommand()) return
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!')
+  }
+})
+
+const TOKEN = process.env.DISCORD_TOKEN
 if (TOKEN) {
-  client.login(TOKEN);
+  client.login(TOKEN)
 } else {
-  throw "No token provided";
+  throw 'No token provided'
 }
