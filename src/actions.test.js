8463cd45bb85a1fdb8b31953146533eb669047f4
Nirina Rabeson <nirinarabeson@gmail.com>
2021-06-13 21:26:45 +0200

    added ping test

--- src/actions.test.js
+++ src/actions.test.js
@@ -1,5 +1,9 @@
 import assert from 'assert'
+import sinon from 'sinon'
 import { Actions } from './actions'
+import { CommandInteraction, Client } from 'discord.js'
+
+const discordClient = new Client({ intents: ['GUILDS', 'GUILD_MESSAGES'] })
 
 describe('Actions', () => {
   /**
@@ -17,6 +21,20 @@ describe('Actions', () => {
         { name: 'claim', description: 'Claim your Paninerd Tokens!' },
       ]
       assert.deepStrictEqual(actions.getCommands(), expectedActions)
+    })
+  })
+
+  describe('#ping', () => {
+    it('Should reply "Tupu!"', () => {
+      const interaction = { reply: () => {} }
+      const spy = sinon.spy(interaction, 'reply').withArgs('Tupu!')
+      actions.ping(interaction)
+      assert(spy.called, "Reply wasn't called")
+      assert.deepStrictEqual(
+        spy.getCall(0).args,
+        ['Tupu!'],
+        'Invalid arguments provided by method',
+      )
     })
   })
 })
